e7e2b25e6d690e97e5198fff9754751c033a31e7
James Dindi <dindijjames@gmail.com>
2018-07-02 23:45:43 +0300

    Basics of using a store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+
+function reducer(state, action){
+    if (action.type === 'changeState'){
+        return action.payload.newState;
+    }
+    return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+    type: 'changeState',
+    payload: {
+        newState: 'New state'
+    }
+};
+
+store.dispatch(action);
+
+console.log(store.getState())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
