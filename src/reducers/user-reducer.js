f7fa3734e629b92e6a85b52bbfc5ddf45dac97a0
James Dindi <dindijjames@gmail.com>
2018-07-03 07:27:18 +0300

    Using mapStateToProps

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -2,6 +2,7 @@ export default function userReducer(state='', { type, payload }){
     switch(type){
         case 'updateUser':
             return payload;
+        default:
+            return state;
     }
-    return state
 }
